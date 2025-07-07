c0fc8da02b42f8786e6889e49552313620f1b85b
avinys <arvydasvinys@gmail.com>
2025-07-07 14:22:01 +0200

    Adding starter components

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Homepage />}></Route>
-                <Route path="product" element={<Product />}></Route>
-                <Route path="pricing" element={<Pricing />}></Route>
-                <Route path="app" element={<AppLayout />}></Route>
-                <Route path="*" element={<PageNotFound />}></Route>
+                <Route path="/" element={<Homepage />} />
+                <Route path="product" element={<Product />} />
+                <Route path="pricing" element={<Pricing />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="app" element={<AppLayout />} />
+                <Route path="*" element={<PageNotFound />} />
             </Routes>
         </BrowserRouter>
     );
