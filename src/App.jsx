ae6865b2fe6e4f9d949dc858465e82700e0d4a70
avinys <arvydasvinys@gmail.com>
2025-07-07 15:56:27 +0200

    Added country list displaying functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
@@ -8,6 +8,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -42,7 +43,7 @@ function App() {
                 <Route path="app" element={<AppLayout />}>
                     <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
                     <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-                    <Route path="countries" element={<p>Countries</p>} />
+                    <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
                     <Route path="form" element={<p>Form</p>} />
                 </Route>
                 <Route path="*" element={<PageNotFound />} />
