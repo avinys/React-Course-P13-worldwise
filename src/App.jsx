ef3535ae9f54ea824112fbfd424f01c251ec6f0e
avinys <arvydasvinys@gmail.com>
2025-07-07 12:26:02 +0200

    Ading starter pages to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
     return (
@@ -12,6 +14,7 @@ function App() {
                 <Route path="/" element={<Homepage />}></Route>
                 <Route path="product" element={<Product />}></Route>
                 <Route path="pricing" element={<Pricing />}></Route>
+                <Route path="app" element={<AppLayout />}></Route>
                 <Route path="*" element={<PageNotFound />}></Route>
             </Routes>
         </BrowserRouter>
