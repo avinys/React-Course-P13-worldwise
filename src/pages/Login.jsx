895ef7f0d9a31cd8871dcd66a6774b977fef38e0
avinys <arvydasvinys@gmail.com>
2025-07-08 20:01:51 +0200

    Added fake authentication context and changed page title with the logo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,17 +2,33 @@ import { useState } from "react";
 
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
+import { useAuth } from "../contexts/fakeAuthContext";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
 
 export default function Login() {
     // PRE-FILL FOR DEV PURPOSES
     const [email, setEmail] = useState("jack@example.com");
     const [password, setPassword] = useState("qwerty");
+    const { login, isAuthenticated } = useAuth();
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (email && password) login(email, password);
+    };
+
+    useEffect(() => {
+        if (isAuthenticated) navigate("/app", { replace: true });
+    }, [isAuthenticated, navigate]);
 
     return (
         <main className={styles.login}>
             <PageNav />
 
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <div className={styles.row}>
                     <label htmlFor="email">Email address</label>
                     <input type="email" id="email" onChange={(e) => setEmail(e.target.value)} value={email} />
@@ -29,7 +45,7 @@ export default function Login() {
                 </div>
 
                 <div>
-                    <button>Login</button>
+                    <Button type="primary">Login</Button>
                 </div>
             </form>
         </main>
